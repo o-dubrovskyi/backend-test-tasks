83355e7f2c971eb860c5cc4331067a7b3110aebf
o-dubrovskyi <oleksandrdubrovskyi@gmail.com>
2022-12-10 10:40:30 +0200

    fix foreign keys

--- database/migrations/2022_12_07_071231_create_orders_products_table.php
+++ database/migrations/2022_12_07_071231_create_orders_products_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('orders_products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('order_id');
+            $table->unsignedBigInteger('order_id');
             $table->foreign('order_id')
                 ->references('id')
                 ->on('orders')
                 ->onDelete('cascade');
-            $table->string('product_id');
+            $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')
                 ->references('id')
                 ->on('products')
--- database/migrations/2022_12_07_071253_create_products_categories_table.php
+++ database/migrations/2022_12_07_071253_create_products_categories_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('products_categories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('product_id');
+            $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')
                 ->references('id')
                 ->on('products')
                 ->onDelete('cascade');
-            $table->string('category_id');
+            $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')
                 ->references('id')
                 ->on('categories')
